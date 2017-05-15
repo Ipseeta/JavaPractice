89f54a0644f4b84ffe1a01c77c17c73eb1cf5002
Ipseeta <ipseeta@hashnode.com>
2017-05-15 17:43:45 +0530

    Huffman and stringpermutation

---
+++ src/string/StringPermutation.java
@@ -0,0 +1,25 @@
+package string;
+
+public class StringPermutation {
+
+	public static void main(String[] args) {
+		perm("ABC");
+	}
+
+	private static void perm(String str){
+		perm("",str);
+	}
+
+	private static void perm(String prefix, String str) {
+		if(null!=str){
+			int n = str.length();
+			if(n == 0) System.out.println(prefix);
+			else{
+				for(int i=0;i<n;i++){
+					perm(prefix + str.charAt(i), str.substring(0, i)+str.substring(i+1, n));
+				}
+			}
+		}
+	}
+
+}
