17f8b167874c53d58da9d916a7791a1511538cf1
Ipseeta <ipseeta@hashnode.com>
2017-05-18 14:42:18 +0530

    Check binary tree is BST or not

---
+++ src/search/BinarySearchTree/Node.java
@@ -0,0 +1,13 @@
+package search.BinarySearchTree;
+
+/**
+ * Created by ipseeta on 5/18/17.
+ */
+public class Node {
+    Node left,right;
+    int data;
+    public Node(int item){
+        data = item;
+        left = right = null;
+    }
+}
